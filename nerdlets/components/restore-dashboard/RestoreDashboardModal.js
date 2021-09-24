3da8f054a9312c651b12b08b0a64c11936bc1c95
Anna Strom-Olsen <aso1124@gmail.com>
2021-09-24 10:41:56 -0400

    feat: add restore modal content
    Add display content for the restore modal.
    
    closes #8

--- nerdlets/components/restore-dashboard/RestoreDashboardModal.js
+++ nerdlets/components/restore-dashboard/RestoreDashboardModal.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react'
 
-import { Modal, HeadingText, BlockText, Button, Spinner } from 'nr1'
+import { Modal, HeadingText, BlockText, Button } from 'nr1'
 export default class RestoreDashboardModal extends Component {
   render() {
     const { onClose, onHideEnd, hidden, dashboard } = this.props
     return (
       <Modal onClose={onClose} onHideEnd={onHideEnd} hidden={hidden}>
-        {JSON.stringify(dashboard)}
+        <div className="modal-container">
+          <HeadingText type={HeadingText.TYPE.HEADING_3}>
+            Restore Dashboard{' '}
+            <strong>
+              {dashboard.accountName}/{dashboard.dashboardName}
+            </strong>
+          </HeadingText>
+          <div className="modal-text-container">
+            <BlockText className="modal-text">
+              This action will restore access to the{' '}
+              <strong>{dashboard.dashboardName}</strong> in the{' '}
+              <strong>{dashboard.accountName}</strong> account.
+            </BlockText>
+            <BlockText className="modal-text">
+              Are you sure you want to continue?
+            </BlockText>
+          </div>
+          <div className="button-row modal-button-row">
+            <Button>Cancel</Button>
+            <Button>Continue</Button>
+          </div>
+        </div>
       </Modal>
     )
   }
