15c61c1780a848870af17ba41ea2a7d5aa4e2647
Anna Strom-Olsen <aso1124@gmail.com>
2021-09-24 15:34:36 -0400

    feat: implement restore dashboard workflow
    Add implementation to restore a selected dashboard.
    
    closes #9, closes #10

--- nerdlets/components/restore-dashboard/RestoreDashboardModal.js
+++ nerdlets/components/restore-dashboard/RestoreDashboardModal.js
@@ -1,12 +1,89 @@
 import React, { Component } from 'react'
 
-import { Modal, HeadingText, BlockText, Button } from 'nr1'
+import {
+  Modal,
+  HeadingText,
+  BlockText,
+  Button,
+  NerdGraphMutation,
+  Icon,
+  Toast,
+} from 'nr1'
 export default class RestoreDashboardModal extends Component {
+  state = {
+    mutating: false,
+    mutationErrors: null,
+  }
+
+  handleRestore = () => {
+    const { dashboard, onClose } = this.props
+    this.setState({ mutating: true, mutationErrors: null }, async () => {
+      const mutation = `mutation {
+      dashboardUndelete(guid: "${dashboard.dashboardGuid}") {
+          errors {
+            description
+            type
+          }
+        }
+      }`
+
+      const { errors } = await NerdGraphMutation.mutate({ mutation })
+      const cleanUp = () => {
+        if (!errors)
+          Toast.showToast({
+            title: `Restored Dashboard`,
+            description: `${dashboard.dashboardName} restored in account ${dashboard.accountName}`,
+            actions: [
+              {
+                label: 'View',
+                onClick: () => console.info('clicked view dashboard'),
+              },
+            ],
+          })
+        onClose(null, dashboard)
+      }
+
+      this.setState({ mutating: false, mutationErrors: errors }, cleanUp())
+    })
+  }
+
   render() {
     const { onClose, onHideEnd, hidden, dashboard } = this.props
+    const { mutating, mutationErrors } = this.state
+
     return (
       <Modal onClose={onClose} onHideEnd={onHideEnd} hidden={hidden}>
         <div className="modal-container">
+          {mutationErrors && (
+            <div className="modal-errors-container">
+              <div className="modal-errors-header-container">
+                <div className="modal-errors-header">
+                  <Icon
+                    color="red"
+                    type={Icon.TYPE.INTERFACE__OPERATIONS__CLOSE__V_ALTERNATE}
+                    className="modal-errors-icon"
+                  />
+                  <p>Sorry, we ran into a problem</p>
+                </div>
+                <BlockText>
+                  Please review the errors below. You can click Continue to try
+                  again, or Cancel to return to the previous screen.
+                </BlockText>
+              </div>
+              <div className="modal-errors-list">
+                {mutationErrors.map(error => (
+                  <ul className="modal-error">
+                    <li>
+                      <div>
+                        <strong>{error.type}</strong>
+                      </div>
+                      <div>{error.description}</div>
+                    </li>
+                  </ul>
+                ))}
+              </div>
+            </div>
+          )}
           <HeadingText type={HeadingText.TYPE.HEADING_3}>
             Restore Dashboard{' '}
             <strong>
@@ -23,9 +100,13 @@ export default class RestoreDashboardModal extends Component {
               Are you sure you want to continue?
             </BlockText>
           </div>
-          <div className="button-row modal-button-row">
-            <Button>Cancel</Button>
-            <Button>Continue</Button>
+          <div className="button-row">
+            <Button onClick={onClose} disabled={mutating}>
+              Cancel
+            </Button>
+            <Button onClick={this.handleRestore} loading={mutating}>
+              Continue
+            </Button>
           </div>
         </div>
       </Modal>
