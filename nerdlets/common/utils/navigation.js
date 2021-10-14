3092a2d75a5e5d89f2c598ad322b778ee8a47f5e
Anna Strom-Olsen <aso1124@gmail.com>
2021-10-14 15:14:42 -0400

    feat: update to use openStackedEntity
    Shift to use openStackedEntity to view dashboards, as it is more correct for this use case

--- nerdlets/common/utils/navigation.js
+++ nerdlets/common/utils/navigation.js
@@ -1,10 +1,7 @@
 import { navigation } from 'nr1'
 
 export const openDashboard = guid => {
-  navigation.openStackedNerdlet({
-    id: 'dashboards.detail',
-    urlState: { entityGuid: guid },
-  })
+  navigation.openStackedEntity(guid)
 }
 
 export const openHistory = dashboard => {
