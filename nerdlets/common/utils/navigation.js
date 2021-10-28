7359f3e6744a2c7b437cbaa891584e62650dd5ee
Anna Strom-Olsen <aso1124@gmail.com>
2021-10-28 11:08:46 -0400

    fix: time period reset to default by dashboard stacked nerdlet
    Opening a dashboard in a stacked nerdlet caused the time period to be reset to default. Adding the "useDefaultTimeRange: false" property to the urlState resolves this issue.
    
    fix #38

--- nerdlets/common/utils/navigation.js
+++ nerdlets/common/utils/navigation.js
@@ -3,7 +3,7 @@ import { navigation } from 'nr1'
 export const openDashboard = guid => {
   navigation.openStackedNerdlet({
     id: 'dashboards.detail',
-    urlState: { entityGuid: guid },
+    urlState: { entityGuid: guid, useDefaultTimeRange: false },
   })
 }
 
